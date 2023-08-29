3501decffd9e9aa51dbdc8ec53e0d3e1eac2adc7
LAPTOP-NELN7LM4\syhuy <huy.t@stor.ai>
2023-08-29 14:22:35 +0700

    generate component product-detail

--- client/src/app/components/shared-components.module.ts
+++ client/src/app/components/shared-components.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TabmenuComponent } from './partials/tabmenu/tabmenu.component';
 import { ProductListComponent } from './product/product-list/product-list.component';
+import { ProductDetailsComponent } from './product/product-details/product-details.component';
+import {RouterModule} from '@angular/router';
 @NgModule({
-  declarations: [TabmenuComponent,ProductListComponent],
-  imports: [CommonModule, IonicModule],
-  exports:[TabmenuComponent, ProductListComponent],
+  declarations: [TabmenuComponent,ProductListComponent,ProductDetailsComponent],
+  imports: [CommonModule, IonicModule,RouterModule],
+  exports:[TabmenuComponent, ProductListComponent,ProductDetailsComponent],
 })
 export class ShareModule {}
