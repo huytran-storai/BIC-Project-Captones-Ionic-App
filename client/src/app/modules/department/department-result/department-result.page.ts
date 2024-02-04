abd76e472a7f91c7843cab08fa6e86f9a04fe417
hieu <trunghieu27102002@gmail.com>
2024-02-04 23:05:55 +0700

    Implement Add and Cancel Button for department result, filter result

--- client/src/app/modules/department/department-result/department-result.page.ts
+++ client/src/app/modules/department/department-result/department-result.page.ts
@@ -3,6 +3,9 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
 import { modalController } from '@ionic/core';
+import { UserService } from 'src/app/services/user.service';
+import { CartService } from 'src/app/services/cart.service';
+import { AlertController, ModalController } from '@ionic/angular';
 @Component({
   selector: 'app-department-result',
   templateUrl: './department-result.page.html',
@@ -18,6 +21,8 @@ export class DepartmentResultPage implements OnInit {
   departmentShow: string[] = [];
   selectedDepartment: any = {};
   showMoreDepartment = false;
+  public user: any;
+  public UserIdCurrent: any;
 
   brands: string[] = [
     `Chimay (1)`,
@@ -47,11 +52,16 @@ export class DepartmentResultPage implements OnInit {
   showMoreLifestyles = false;
   public productData: any;
   productResult: any[] = [];
+  public renderStrapiId: any;
 
   constructor(
     private route: ActivatedRoute,
     private productService: StoreService,
-    private router: Router
+    private router: Router,
+    private userService: UserService,
+    private CartService: CartService,
+    private modalController: ModalController,
+    public alertController: AlertController,
   ) {
     this.departments.forEach((department) => {
       this.selectedDepartment[department] = false;
@@ -73,16 +83,30 @@ export class DepartmentResultPage implements OnInit {
     this.renderProductResult();
   }
 
+  getUserData() {
+    this.userService.getUserData().subscribe(
+      (res) => {
+        this.user = res?.user;
+        console.log('find user: ', this.user);
+        this.UserIdCurrent = this.user.UserId;
+        console.log('find UserIdCurrent: ', this.UserIdCurrent);
+      },
+      (error) => {
+        console.log('Get user data error', error);
+      }
+    );
+  }
+
   renderProductResult() {
     this.productService.getProducts().subscribe(
       (res: any) => {
         this.route.params.subscribe((params) => {
           this.tagName = params['tagName'];
         });
-        this.productData = res.data.map((item: any) => item.attributes);
+        this.productData = res.data.map((item: any) => item);
         if (this.productData && this.productData.length > 0) {
           this.productResult = this.productData.filter((product: any) => {
-            return product.DrinkType === this.tagName;
+            return product.attributes.DrinkType === this.tagName;
           });
         } else {
           console.log('No data');
@@ -140,25 +164,90 @@ export class DepartmentResultPage implements OnInit {
   }
 
   navigateToProductDetail(item: any) {
-    this.router.navigate(['product-detail/', item.ProductName,item.ProductId]);
+    this.router.navigate(['product-detail/', item.attributes.ProductName,item.attributes.ProductId]);
   }
 
-  addProduct(event: Event,items: any) {
+  addProduct(event: Event, item: any) {
+    if (this.user !== undefined && this.user !== null) {
     event.stopPropagation();
-      this.itemCart = JSON.parse(localStorage.getItem('localCart') || '[]');
-        this.itemCart.push(items);
-        localStorage.setItem('localCart', JSON.stringify(this.itemCart));
+    const productData = {
+      ProductName: item.attributes.ProductName,
+      ProductPrice: item.attributes.ProductPrice,
+      QuantityDefault: 1,
+      ProductImage: item.attributes.ProductImage,
+      ProductId: item.attributes.ProductId,
+      OrderedUserId: this.UserIdCurrent,
+    };
+    this.CartService.pushProducts(productData).subscribe(
+      (response) => {
+        const strapiId = response.data.id;
+        const saveProductId = response.data.attributes.ProductId;
+        const savedCartItemsString = localStorage.getItem('saveCartItems');
+        const existingCartItems = savedCartItemsString? JSON.parse(savedCartItemsString): [];
+        existingCartItems.push({ ...item, strapiId, saveProductId });
+        localStorage.setItem('saveCartItems',JSON.stringify(existingCartItems));
+        this.renderStrapiId = existingCartItems;
+        window.location.reload();
+      },
+      (error) => {
+        console.error('Error adding product to cart:', error);
+      }
+    );
+    }
+    else {
+      event.stopPropagation();
+      this.alertController
+        .create({
+          header: 'Thông báo',
+          message: 'Vui lòng đăng nhập để thêm sản phẩm',
+          buttons: [
+            {
+              text: 'Đăng nhập',
+              handler: () => {
+                this.modalController.dismiss();
+                this.router.navigate(['./login']);
+                setTimeout(() => {
+                  window.location.reload();
+                }, 0);
+              },
+            },
+          ],
+        })
+        .then((alert) => {
+          alert.present();
+        });
+    }
   }
 
-  cancelProduct(event: Event,items: any){
+
+  cancelProduct(event: Event, item: any) {
     event.stopPropagation();
-    let cartData = JSON.parse(localStorage.getItem('localCart') || '[]')
-    cartData = cartData.filter((product: any) => product.ProductId !== items.ProductId)
-    localStorage.setItem('localCart', JSON.stringify(cartData))
+    // console.log('this.renderStrapiId in cancel button', this.renderStrapiId);
+    const cartItem = this.renderStrapiId.find((cart: any) => cart.saveProductId === item.attributes.ProductId);
+    if (cartItem) {
+      const strapiIdToDelete = cartItem.strapiId;
+      // console.log('strapiIdToDelete:', strapiIdToDelete);
+      if (strapiIdToDelete) {
+        this.CartService.deleteProduct(strapiIdToDelete).subscribe(
+          (response) => {
+            // console.log('Product deleted from cart successfully:', response);
+            this.renderStrapiId = this.renderStrapiId.filter((cart: any) => cart.saveProductId !== item.attributes.ProductId);
+            localStorage.setItem('saveCartItems', JSON.stringify(this.renderStrapiId));
+          },
+          (error) => {
+            console.error('Error deleting product from cart:', error);
+          }
+        );
+      } else {
+        console.error('Strapi ID is not available. Unable to delete product.');
+      }
+    } else {
+      console.error('CartItem not found in renderStrapiId.');
+    }
   }
 
-  isProductInCart(items: any):boolean{
-    let cartData = JSON.parse(localStorage.getItem('localCart') || '[]')
-    return cartData.some((product: any) => product.ProductId === items)
-  }
+  isProductInCart(item: number): boolean{
+    let cartData = JSON.parse(localStorage.getItem('saveCartItems') || '[]')
+    return cartData.some((product: any) => product.saveProductId === item)
+}
 }
--- client/src/app/modules/department/department-result/filter-result/filter-result.page.ts
+++ client/src/app/modules/department/department-result/filter-result/filter-result.page.ts
@@ -3,8 +3,8 @@ import { StoreService } from 'src/app/services/store.service';
 import { ProductItem } from 'src/app/shared/models/ProductItem';
 import { Router } from '@angular/router';
 import { CartService } from 'src/app/services/cart.service';
-import { NavController } from '@ionic/angular';
-
+import { NavController,AlertController,ModalController } from '@ionic/angular';
+import { UserService } from 'src/app/services/user.service';
 @Component({
   selector: 'app-filter-result',
   templateUrl: './filter-result.page.html',
@@ -12,18 +12,42 @@ import { NavController } from '@ionic/angular';
 })
 export class FilterResultPage implements OnInit {
   items: ProductItem[] = [];
-  productData: any;
+  public productData: any;
+  public user: any;
+  public UserIdCurrent: any;
+  public renderStrapiId: any;
 
   constructor(
     private StoreService: StoreService,
     private router: Router,
     private CartService: CartService,
-    private navCtrl: NavController
+    private navCtrl: NavController,
+    public alertController: AlertController,
+    private modalController: ModalController,
+    private productService: StoreService,
+    private userService: UserService
   ) { }
 
   ngOnInit() {
     this.items = this.StoreService.getAllProducts();
     this.getProductRender()
+    this.getUserData();
+    const initializationCart = localStorage.getItem('saveCartItems');
+    this.renderStrapiId = initializationCart? JSON.parse(initializationCart): [];
+  }
+
+  getUserData() {
+    this.userService.getUserData().subscribe(
+      (res) => {
+        this.user = res?.user;
+        console.log('find user: ', this.user);
+        this.UserIdCurrent = this.user.UserId;
+        console.log('find UserIdCurrent: ', this.UserIdCurrent);
+      },
+      (error) => {
+        console.log('Get user data error', error);
+      }
+    );
   }
 
   Back(){
@@ -31,27 +55,93 @@ export class FilterResultPage implements OnInit {
   }
 
   navigateToProductDetail(items: any) {
-    this.router.navigate(['product-detail/', items.ProductName,items.ProductId]); 
+    this.router.navigate(['product-detail/', items.attributes.ProductName,items.attributes.ProductId]); 
   }
 
   itemCart:any = []
-  addProduct(event: Event,items: any){
+  addProduct(event: Event, item: any) {
+    if (this.user !== undefined && this.user !== null) {
     event.stopPropagation();
-      this.itemCart = JSON.parse(localStorage.getItem('localCart') || '[]');
-        this.itemCart.push(items)
-        localStorage.setItem('localCart', JSON.stringify(this.itemCart))
-  }
-  cancelProduct(event: Event,items: any) {
-    event.stopPropagation();
-    let cartData = JSON.parse(localStorage.getItem('localCart') || '[]')
-    cartData = cartData.filter((product: any) => product.ProductId !== items.ProductId)
-    localStorage.setItem('localCart', JSON.stringify(cartData))
+    const productData = {
+      ProductName: item.attributes.ProductName,
+      ProductPrice: item.attributes.ProductPrice,
+      QuantityDefault: 1,
+      ProductImage: item.attributes.ProductImage,
+      ProductId: item.attributes.ProductId,
+      OrderedUserId: this.UserIdCurrent,
+    };
+    this.CartService.pushProducts(productData).subscribe(
+      (response) => {
+        const strapiId = response.data.id;
+        const saveProductId = response.data.attributes.ProductId;
+        const savedCartItemsString = localStorage.getItem('saveCartItems');
+        const existingCartItems = savedCartItemsString? JSON.parse(savedCartItemsString): [];
+        existingCartItems.push({ ...item, strapiId, saveProductId });
+        localStorage.setItem('saveCartItems',JSON.stringify(existingCartItems));
+        this.renderStrapiId = existingCartItems;
+        window.location.reload();
+      },
+      (error) => {
+        console.error('Error adding product to cart:', error);
+      }
+    );
+    }
+    else {
+      event.stopPropagation();
+      this.alertController
+        .create({
+          header: 'Thông báo',
+          message: 'Vui lòng đăng nhập để thêm sản phẩm',
+          buttons: [
+            {
+              text: 'Đăng nhập',
+              handler: () => {
+                this.modalController.dismiss();
+                this.router.navigate(['./login']);
+                setTimeout(() => {
+                  window.location.reload();
+                }, 0);
+              },
+            },
+          ],
+        })
+        .then((alert) => {
+          alert.present();
+        });
+    }
   }
 
-  isProductInCart(productId: any): boolean {
-    let cartData = JSON.parse(localStorage.getItem('localCart') || '[]')
-    return cartData.some((product: any) => product.ProductId === productId)
+  cancelProduct(event: Event, item: any) {
+    event.stopPropagation();
+    // console.log('this.renderStrapiId in cancel button', this.renderStrapiId);
+    const cartItem = this.renderStrapiId.find((cart: any) => cart.saveProductId === item.attributes.ProductId);
+    if (cartItem) {
+      const strapiIdToDelete = cartItem.strapiId;
+      // console.log('strapiIdToDelete:', strapiIdToDelete);
+      if (strapiIdToDelete) {
+        this.CartService.deleteProduct(strapiIdToDelete).subscribe(
+          (response) => {
+            // console.log('Product deleted from cart successfully:', response);
+            this.renderStrapiId = this.renderStrapiId.filter((cart: any) => cart.saveProductId !== item.attributes.ProductId);
+            localStorage.setItem('saveCartItems', JSON.stringify(this.renderStrapiId));
+          },
+          (error) => {
+            console.error('Error deleting product from cart:', error);
+          }
+        );
+      } else {
+        console.error('Strapi ID is not available. Unable to delete product.');
+      }
+    } else {
+      console.error('CartItem not found in renderStrapiId.');
+    }
   }
+
+
+  isProductInCart(item: number): boolean{
+    let cartData = JSON.parse(localStorage.getItem('saveCartItems') || '[]')
+    return cartData.some((product: any) => product.saveProductId === item)
+}
 
   cartNumber: number = 0;
   cartNumberFunc() {
@@ -65,10 +155,10 @@ export class FilterResultPage implements OnInit {
   }
   
   getProductRender() {
-    this.StoreService.getProducts().subscribe(
+    this.productService.getProducts().subscribe(
       (res: any) => {
-        this.productData = res.data.map((item: any) => item.attributes);
-        console.log("Product lists:", this.productData)
+        this.productData = res.data.map((item: any) => item);
+        console.log('Product lists:', this.productData);
       },
       (err: any) => {
         console.error('Error fetching current store data:', err);
